48891b334b6d811cc1980dd63000b2feadd5e8e4
JunSeok <ppu1234@naver.com>
2022-11-25 15:00:07 +0900

    Recursion

---
+++ func_pro3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int multiply(int N, int sum, int i);
+
+int main()
+{
+    int N;
+    int sum = 1;
+    int i = 1;
+    scanf("%d", &N);
+
+    printf("1부터 N까지 곱한 값 : %d\n", multiply(N, sum, i));
+    return 0;
+}
+
+int multiply(int N, int sum, int i)
+{
+    if( i <= N) {
+        sum *= i;
+        i++;
+       return  multiply(N, sum, i);
+    } else return sum;
+}
