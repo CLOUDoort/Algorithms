196d0f3583fc92be562af1ad93722ed970448c8c
JunSeok <ppu1234@naver.com>
2022-12-28 12:49:38 +0900

    baekjoon 2490

--- 0x02/10.cpp
+++ 0x02/10.cpp
@@ -1,25 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int input, result;
-string res = "DCBAE";
-
-int main(void){
+int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-    // A : 0111 도 3
-    // B : 0011 개 2
-    // C : 0001 걸 1
-    // D : 0000 윳 0
-    // E : 1111 모 4
-    for(int r = 0; r < 3; r++) {
+
+    string res = "DCBAE";
+    int input, result;
+
+    for(int c = 0; c < 3; c++) {
         result = 0;
-        for(int c = 0; c < 4; c++) {
+        for(int r = 0; r < 4; r++) {
             cin >> input;
             result += input;
         }
         cout << res[result] << '\n';
     }
-    
-    
+    return 0;
+
 }
