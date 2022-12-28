71e3d3c200d635052784447dcd0c8c9a2ab052de
JunSeok <ppu1234@naver.com>
2022-12-28 15:32:09 +0900

    baekjoon 2309 - better_solution

--- 0x02/13.cpp
+++ 0x02/13.cpp
@@ -4,29 +4,21 @@ using namespace std;
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int a[9], result[7], sum = 0, value = 0, n = 0;
-    int delete_num[2];
+    int a[9], sum = 0;
     for(int i = 0; i < 9; i++) {
         cin >> a[i];
         sum += a[i];
     }
-    value = sum - 100;
-    for(int i = 0; i < 9; i++) {
-        for(int j = i+1; j < 9; j++) {
-            if(a[i] + a[j] == value) {
-                delete_num[0] = a[i];
-                delete_num[1] = a[j];
-                break;
+    sort(a, a+9);
+    for(int i = 0; i < 8; i++) {
+        for(int j = 1; j < 9; j++) {
+            if(sum - a[i] - a[j] == 100) {
+                for(int k = 0; k < 9; k++) {
+                    if(k == i || k == j) continue;
+                    cout << a[k] << '\n';
+                }
+                return 0;
             }
         }
-    }
-    for(int i = 0; i < 9; i++) {
-        if(a[i] == delete_num[0] || a[i] == delete_num[1]) continue;
-        result[n] = a[i];
-        n++;
-    }
-    sort(result, result+7);
-    for(int i = 0; i < 7; i++) {
-        cout << result[i] << '\n';
     }
 }
