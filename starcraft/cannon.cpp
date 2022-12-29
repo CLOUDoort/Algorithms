e48871db6818187c473227f815c214531058d826
JunSeok <ppu1234@naver.com>
2022-12-29 13:28:48 +0900

    copy_constructor

--- starcraft/cannon.cpp
+++ starcraft/cannon.cpp
@@ -6,14 +6,37 @@ class Photon_Cannon {
     int coord_x, coord_y;
     int damage;
 
-    public: 
+    public:
         Photon_Cannon(int x, int y);
-        Photon_Cannon(const Photon_Cannon& pc);
+        Photon_Cannon(const Photon_Cannon& pc); // 복사 생성자의 표준적 정의, const 이기 때문에 복사 생성자 내부에서 데이터 변경 불가능
 
         void show_status();
 };
 Photon_Cannon::Photon_Cannon(const Photon_Cannon& pc) {
     std::cout << "복사 생성자 호출!" << std::endl;
     hp = pc.hp;
-    
+    shield = pc.shield;
+    coord_x = pc.coord_x;
+    coord_y = pc.coord_y;
+    damage = pc.damage;
+}
+Photon_Cannon::Photon_Cannon(int x, int y) {
+    std::cout << "생성자 호출!" << std::endl;
+    hp = shield = 100;
+    coord_x = x;
+    coord_y = y;
+    damage = 28;
+}
+void Photon_Cannon::show_status() {
+    std::cout << "Photon Cannon " << std::endl;
+    std::cout << " Location : ( " << coord_x << " , " << coord_y << " ) " << std::endl;
+    std::cout << " HP : " << hp << std::endl;
+}
+
+int main() {
+    Photon_Cannon pc1(3, 3);
+    Photon_Cannon pc2(pc1);
+    Photon_Cannon pc3(pc2);
+    pc1.show_status();
+    pc2.show_status();
 }
