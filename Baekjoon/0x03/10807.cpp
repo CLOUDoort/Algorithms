29c81fe8cd26a3c599f94e0727e85aca66ea3ce3
JunSeok <ppu1234@naver.com>
2023-01-02 17:41:41 +0900

    baekjoon 10807 best

--- Baekjoon/0x03/10807.cpp
+++ Baekjoon/0x03/10807.cpp
@@ -6,13 +6,12 @@ int check[300] = {};
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int N, v, t, value = 0;
+    int N, v, t;
     cin >> N;
     for(int i = 0; i < N; i++) {
         cin >> t;
         if(t >= -100 && t <= 100) check[t+100]++;
     }
     cin >> v;
-    if(v >= -100 && v <= 100) value = check[v+100];
-    cout << value; 
+    cout << check[v+100]; 
 }
