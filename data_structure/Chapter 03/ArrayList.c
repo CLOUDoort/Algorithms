44f4be03673756a65d0849328fed1f719ca39a4a
JunSeok <ppu1234@naver.com>
2023-01-02 15:36:34 +0900

    ArrayList

--- data_structure/Chapter 03/ArrayList.c
+++ data_structure/Chapter 03/ArrayList.c
@@ -1,3 +1,10 @@
+// ArrarList의 ADT
+// void ListInit(List *plist);
+// void LInsert(List *plist, LData data);
+// int LFirst(List *plist, LData *pdata); , true / false
+// int LNext(List *plist, LData *pdata); , true / false 
+// LData LRemove(List *plist); , 삭제된 데이터 반환
+// int LCount(List *plist);
 #include <stdio.h>
 #include "ArrayList.h"
 
@@ -5,39 +12,38 @@ void ListInit(List *plist) {
     plist->numOfData = 0;
     plist->curPosition = -1;
 }
-
 void LInsert(List *plist, LData data) {
     if(plist->numOfData >= LIST_LEN) {
-        puts("저장이 불가능합니다! \n");
+        puts("저장공간이 꽉 찼습니다! \n");
         return;
     }
     plist->arr[plist->numOfData] = data;
     plist->numOfData++;
 }
+
 int LFirst(List *plist, LData *pdata) {
     if(plist->numOfData == 0) return FALSE;
-    plist->curPosition = 0;
     *pdata = plist->arr[0];
+    plist->curPosition = 0;
     return TRUE;
 }
 int LNext(List *plist, LData *pdata) {
-    if(plist->curPosition >= plist->numOfData -1) return FALSE;
+    if(plist->curPosition >= plist->numOfData-1) return FALSE;
     plist->curPosition++;
     *pdata = plist->arr[plist->curPosition];
     return TRUE;
 }
 
 LData LRemove(List *plist) {
-    int rpos = plist->curPosition;
-    int num = plist->numOfData;
-    int i;
-    LData rdata = plist->arr[rpos];
-
-    for(i = rpos; i < num-1; i++) plist->arr[i] = plist->arr[i+1];
+    LData data = plist->arr[plist->curPosition];
+    for(int i = plist->curPosition; i < plist->numOfData-1; i++) {
+        plist->arr[i] = plist->arr[i+1];
+    }
     plist->curPosition--;
     plist->numOfData--;
-    return rdata;
+    return data;
 }
+
 int LCount(List *plist) {
     return plist->numOfData;
 }
--- data_structure/Chapter 03/ArrayList.h
+++ data_structure/Chapter 03/ArrayList.h
@@ -1,4 +1,5 @@
-#ifndef __ARRAY_LIST_H__
+// __ARRAY_LIST_H__가 정의되지 않으면 실행
+#ifndef __ARRAY_LIST_H__ 
 #define __ARRAY_LIST_H__
 
 #define TRUE 1
@@ -8,20 +9,21 @@
 typedef int LData;
 
 typedef struct __ArrayList {
-    LData arr[LIST_LEN]; // 리스트의 저장소인 배열
-    int numOfData;  // 저장된 데이터의 수
-    int curPosition;  // 데이터의 참조위치를 기록
+    LData arr[LIST_LEN];
+    int numOfData;
+    int curPosition;
 } ArrayList;
 
 typedef ArrayList List;
 
-void ListInit(List *plist); // 초기화
-void LInsert(List *plist, LData data); // 데이터 저장
+void ListInit(List *plist);
+void LInsert(List *plist, LData data);
 
-int LFirst(List *plist, LData *pdata); // 첫 데이터 참조
-int LNext(List *plist, LData *pdata); // 두 번째 이후 데이터 참조
 
-LData LRemove(List *plist); // 참조한 데이터 삭제
-int LCount(List *plist); // 저장된 데이터의 수 반환
+int LFirst(List *plist, LData *pdata);
+int LNext(List *plist, LData *pdata);
+LData LRemove(List *plist);
+int LCount(List *plist);
+
 
 #endif
