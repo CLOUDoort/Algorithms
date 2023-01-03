295494114852f9667b0d77ae5c2745bcdc526d39
JunSeok <ppu1234@naver.com>
2023-01-03 16:00:07 +0900

    baekjoon 1919 My_solution

---
+++ Baekjoon/0x03/1919.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    string s1, s2;
+    int a[26] = {}, value = 0;
+    cin >> s1 >> s2;
+    for(char c : s1) a[c-'a']++;
+    for(char c : s2) a[c-'a']--;
+    
+    for(int i : a) {
+        if(i != 0) {
+            if(i >= 0) value += i;
+            else value -= i;
+        } 
+    }
+    cout << value;
+}
