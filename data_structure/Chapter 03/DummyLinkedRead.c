8e4092f75174808324882ac0b0a496255691bd36
JunSeok <ppu1234@naver.com>
2023-01-05 20:47:47 +0900

    dummy LinkedRead

--- data_structure/Chapter 03/DummyLinkedRead.c
+++ data_structure/Chapter 03/DummyLinkedRead.c
@@ -14,6 +14,8 @@ int main(void) {
     Node *newNode = NULL;
     int readData;
 
+    head = (Node*)malloc(sizeof(Node)); // 더미노드 추가
+    tail = head;
     // 데이터 입력과정
     while(1) {
         printf("데이터 입력 : ");
@@ -24,24 +26,17 @@ int main(void) {
         newNode = (Node*)malloc(sizeof(Node));
         newNode->data = readData;
         newNode->next = NULL;
-
-        if(head == NULL) {
-            head = newNode;
-            tail = newNode;
-        }
-        else  {
-            newNode->next = head;
-            head = newNode;
-        }
+        
+        tail->next = newNode;
+        tail = newNode;
     }
     printf("\n");
 
     // 입력받은 데이터 출력과정
     printf("데이터 전체 출력! \n");
-    if(head == NULL) printf("출력할 데이터가 없습니다! \n");
+    if(head == tail) printf("저장된 자연수가 존재하지 않습니다. \n");
     else {
         cur = head;
-        printf("%d ", cur->data);
         while(cur->next != NULL) {
             cur = cur->next;
             printf("%d ", cur->data);
@@ -50,12 +45,10 @@ int main(void) {
     printf("\n\n");
 
     // 메모리 해제 과정 //
-    if(head == NULL) return 0;
+    if(head == tail) return 0;
     else {
         Node* deleteNode = head;
         Node* deleteNextNode = head->next;
-        printf("%d를 삭제합니다! \n", head->data);
-        free(deleteNode);
         while(deleteNextNode != NULL) {
             deleteNode = deleteNextNode;
             deleteNextNode = deleteNextNode->next;
