0e3c65041cdeefaa59b9b03feeea75ab0982786c
JunSeok <ppu1234@naver.com>
2023-01-08 16:12:42 +0900

    baekjoon 1406 야매

--- Baekjoon/0x04/1406-야매.cpp
+++ Baekjoon/0x04/1406-야매.cpp
@@ -2,23 +2,45 @@
 using namespace std;
 
 const int MX = 1000005;
-char dat[MX], pre[MX], nxt[MX];
+char dat[MX];
+int pre[MX], nxt[MX];
 int unused = 1;
 
-void insert(int addr, char b) {
-    dat[unused] = b;
+void insert(int addr, char c) {
+    dat[unused] = c;
+    pre[unused] = addr;
+    nxt[unused] = nxt[addr];
+    if(nxt[addr] != -1) pre[nxt[addr]] = unused;
+    nxt[addr] = unused;
+    unused++;
+}
+
+void erase(int addr) {
+    nxt[pre[addr]] = nxt[addr];
+    if(nxt[addr] != -1) pre[nxt[addr]] = pre[addr];
+    // 메모리 해제가 안됨
+}
+
+void traversal() {
+    int cur = nxt[0];
+    while(cur != -1) {
+        cout << dat[cur];
+        cur = nxt[cur];
+    }
 }
 
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
+    fill(pre, pre+MX, -1);
+    fill(nxt, nxt+MX, -1);
     string N;
     cin >> N;
-    for(int i = 0; i < N.length(); i++) {
-        dat[unused] = N[i];
-        unused++; 
+    int cursor = 0;
+    for(auto c : N) {
+        insert(cursor, c);
+        cursor++;
     }
-    int cursur = nxt[unused-1];
     int M;
     cin >> M;
     while(M--) {
@@ -27,10 +49,25 @@ int main(void) {
         if(a == 'P') {
             char b;
             cin >> b;
-            insert(dat[cursur], b);
+            insert(cursor, b);
+            cursor = nxt[cursor];
+        }
+        else if(a == 'L') {
+            if(pre[cursor] != -1)  {
+                cursor = pre[cursor];
+            }
+        }
+        else if(a == 'D') {
+            if(nxt[cursor] != -1) {
+                cursor = nxt[cursor];
+            }
+        }
+        else {
+            if(pre[cursor] != -1) {
+                erase(cursor);
+                cursor = pre[cursor];
+            }
         }
     }
-
-    fill(pre, pre+MX, -1);
-    fill(nxt, nxt+MX, -1);
+    traversal();
 }
