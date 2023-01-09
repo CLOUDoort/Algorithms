d6e603dd21dcfe5499bf75e3bd816cf40185cccd
JunSeok <ppu1234@naver.com>
2023-01-10 00:49:15 +0900

    baekjoon 5397

--- Baekjoon/0x04/5397.cpp
+++ Baekjoon/0x04/5397.cpp
@@ -4,11 +4,31 @@ using namespace std;
 int main(void){
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int N;
+    int N; // the number of test case
     cin >> N;
     while(N--) {
         string s;
+        list<char> L;
         cin >> s;
-        
+        auto cur = L.begin();
+        for(auto c : s) {
+            if(c == '<') {
+                if(cur != L.begin()) cur--;
+            }
+            else if(c == '>') {
+                if(cur != L.end()) cur++;
+            }
+            else if(c == '-') {
+                if(cur != L.begin()) {
+                    cur--;
+                    cur = L.erase(cur);
+                }
+            }
+            else {
+                L.insert(cur, c);
+            }
+        }
+        for(auto i : L) cout << i;
+        cout << '\n';
     }
 }
