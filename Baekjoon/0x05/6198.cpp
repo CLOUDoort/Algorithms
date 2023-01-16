86c53215ba2826733355f215c42ab00fb8b25bfc
JunSeok <ppu1234@naver.com>
2023-01-16 20:06:28 +0900

    baekjoon 6198

---
+++ Baekjoon/0x05/6198.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+stack<int> s;
+int N, ans;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N;
+    ll height;
+    while(N--) {
+        cin >> height;
+        while(!s.empty() && s.top() <= height) s.pop(); 
+        ans += s.size();
+        s.push(height);
+    }
+    cout << ans;
+    return 0;
+}
