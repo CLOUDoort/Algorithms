4d9d52e550048c4b7ed50d576810aecdb5fe55ff
JunSeok <ppu1234@naver.com>
2023-01-29 23:58:10 +0900

    baekjoon 4949

---
+++ Baekjoon/0x08/4949.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    while(1) {
+        stack<char> s;
+        string a;
+        cin >> a;
+        if(a == ".") return 0;
+        for(int i = 0; i < a.length(); i++) {
+            if(a[i] == '(') {
+
+            }
+            else if(a[i] == '[') {
+
+            }
+            else continue;
+        }
+    }
+}
