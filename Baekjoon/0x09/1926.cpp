8b4e02e593ded6bb6be5f95ea2e0ea03f36ccb2c
JunSeok <ppu1234@naver.com>
2023-02-07 11:22:21 +0900

    :technologist: baekjoon 1926 practice

--- Baekjoon/0x09/1926.cpp
+++ Baekjoon/0x09/1926.cpp
@@ -2,34 +2,35 @@
 using namespace std;
 #define X first
 #define Y second
-
 int board[502][502];
-bool vis[502][502];
-int m, n;
+int vis[502][502];
+int n, m;
 int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, 1, 0, 1};
+int dy[4] = {0, 1, 0, -1};
 
 int main(void){
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin >> n >> m;
     for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
+        for(int j = 0; j < m; j++){
             cin >> board[i][j];
         }
     }
-    int mx = 0; // 그림의 최댓값
-    int num = 0; // 그림의 수
-    
+    int mx = 0;
+    int num = 0;
     for(int i = 0; i < n; i++){
         for(int j = 0; j < m; j++){
-            if(board[i][j] == 0 || vis[i][j]) continue;
-            num++;
+            int area = 0;
+            if(vis[i][j] || board[i][j] == 0) continue;
             queue<pair<int, int>> Q;
+            // BFS 시작지점
             vis[i][j] = 1;
             Q.push({i, j});
-            int area = 0;
+            num++;
+            // BFS 돌리기
             while(!Q.empty()){
+                // pop이 이루어질 때마다 area++
                 area++;
                 pair<int, int> cur = Q.front(); Q.pop();
                 for(int dir = 0; dir < 4; dir++){
@@ -46,3 +47,11 @@ int main(void){
     }
     cout << num << '\n' << mx;
 }
+
+/*
+- 아이디어
+    - DFS 사용
+    - board 전부 다 돌아야함 => 이중for문
+    - 이중 for문을 통해 BFS 돌릴 좌표 찾고 BFS돌리기, 그림 개수++
+    - DFS돌릴 때 그림 넓이 구하기, 그림 넓이 최댓값은 DFS 돌릴 떄마다 max돌리기
+*/
