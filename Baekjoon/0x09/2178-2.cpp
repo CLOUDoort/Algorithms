fee2ae2a73a29605b3ccd9b8f54e740989da7af6
JunSeok <ppu1234@naver.com>
2023-02-12 00:36:24 +0900

    :technologist: baekjoon 2178 practice

---
+++ Baekjoon/0x09/2178-2.cpp
@@ -0,0 +1,39 @@
+/*
+아이디어
+- 처음부터 끝까지 최단거리 구하는 문제
+- 각 수가 붙어서 주어짐 -> string 배열로 받기
+- 2차원 배열 만들어서 방문표시 겸 거리 표시
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+string board[102];
+int dist[102][102];
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+int n ,m;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> m;
+    for(int i = 0; i < n; i++) cin >> board[i];
+    for(int i = 0; i < n; i++) fill(dist[i], dist[i]+m, -1);
+    queue<pair<int, int>> Q;
+    dist[0][0] = 0;
+    Q.push({0, 0});
+    while(!Q.empty()) {
+        auto cur = Q.front(); Q.pop();
+        for(int dir = 0; dir < 4; dir++) {
+            int nx = cur.X + dx[dir];
+            int ny = cur.Y + dy[dir];
+            if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+            if(dist[nx][ny] >= 0 || board[nx][ny] != '1') continue;
+            dist[nx][ny] = dist[cur.X][cur.Y] + 1;
+            Q.push({nx, ny});
+        }
+    }
+    cout << dist[n-1][m-1] + 1;
+}
