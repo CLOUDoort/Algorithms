085c77ec19f787f6956ab52ddc789808ea865c99
JunSeok <ppu1234@naver.com>
2023-02-14 03:32:31 +0900

    :technologist: Selection Sort

--- data_structure/Chapter 10/SelectionSort.c
+++ data_structure/Chapter 10/SelectionSort.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-void SelSort(int arr[], int n)  {
-    int i, j;
-    int maxIdx;
-    int temp;
+void SelSort(int arr[], int n) {
+    int i, j, temp, maxIdx;
 
+    // 오름차순일 경우 첫번째 자료를 선택해서 나머지 자료들과 비교하여 최솟값을 첫번째 자리와 교체
+    // 이를 반복
     for(i = 0; i < n-1; i++) {
         maxIdx = i;
-
         for(j = i+1; j < n; j++) {
             if(arr[j] < arr[maxIdx]) maxIdx = j;
         }
