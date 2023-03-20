bd19483d8f5159b8fa8dca48db0f11f3a22cf885
JunSeok <ppu1234@naver.com>
2023-03-20 13:26:45 +0900

    :art: sparse_matrix

--- Data_structure(학부)/practice/sparse_matrix.c
+++ Data_structure(학부)/practice/sparse_matrix.c
@@ -25,6 +25,27 @@ void Compress(int SparseMatrix[][6], int row, int col, int cnt, Element CompMatr
     }
 }
 
+void smTranspose(Element a[], Element b[]) {
+    int currentb;
+    b[0].col = a[0].row;
+    b[0].row = a[0].col;
+    b[0].value = a[0].value;
+
+    if(a[0].value > 0) {
+        currentb = 1;
+        for(int i = 0; i < a[0].col; i++) {
+            for(int j = 1; j <= a[0].value; j++) {
+                if(a[j].col == i) {
+                    b[currentb].row = a[j].col;
+                    b[currentb].col = a[j].row;
+                    b[currentb].value = a[j].value;
+                    currentb++;
+                }
+            }
+        }
+    }
+}
+
 int main() {
     int SparseMatrix[6][6] = {
         {15, 0, 0, 22, 0, -15},
@@ -41,9 +62,15 @@ int main() {
         }
     }
     Element* CompMatrix = (Element*)malloc(sizeof(Element) * (cnt+1));
+    Element* TransposeMatrix = (Element*)malloc(sizeof(Element) * (cnt+1));
 
     Compress(SparseMatrix, 6, 6, cnt, CompMatrix);
     for(int i = 0; i < cnt+1; i++) {
         printf("row: %d col : %d value: %d\n", CompMatrix[i].row, CompMatrix[i].col, CompMatrix[i].value);
+    }
+    smTranspose(CompMatrix, TransposeMatrix);
+    printf("\n-------------------\n");
+    for(int i = 0; i < cnt+1; i++) {
+        printf("row: %d col : %d value: %d\n", TransposeMatrix[i].row, TransposeMatrix[i].col, TransposeMatrix[i].value);
     }
 }
