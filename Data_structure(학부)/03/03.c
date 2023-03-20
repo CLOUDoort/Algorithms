70a8f4a0fa84690816ae92178745a7633e7f7a63
JunSeok <ppu1234@naver.com>
2023-03-20 09:38:25 +0900

    :pencil2: fixed

--- Data_structure(학부)/03/03.c
+++ Data_structure(학부)/03/03.c
@@ -14,7 +14,7 @@ int main() {
     }
     int *arr1 = (int*)malloc(sizeof(int)*10);
     int n1 = 0;
-    int cnt1 = 0;
+    int cnt1 = 10;
     while(fscanf(fp1, "%d", &arr1[n1]) != EOF) {
         n1++;
         // 데이터 개수가 10의 배수일 때
@@ -22,17 +22,17 @@ int main() {
         if(n1 % 10 == 0) {
             if(!feof(fp1)) {
             // 110개 이상 할당하지 않은 경우, 10개 다시 할당
-                if(cnt1 < 11) {
-                    int *new_arr1 = (int*)realloc(arr1, 10*sizeof(int));
+                if(cnt1 < 110) {
+                    int *new_arr1 = (int*)realloc(arr1, cnt1*sizeof(int));
                     arr1 = new_arr1;
-                    cnt1++;
+                    cnt1 += 10;
                 }
             }
         }
     }
     int *arr2 = (int*)malloc(sizeof(int)*10);
     int n2 = 0;
-    int cnt2 = 0;
+    int cnt2 = 10;
     while(fscanf(fp2, "%d", &arr2[n2]) != EOF) {
         n2++;
         // 데이터 개수가 10의 배수일 때
@@ -40,10 +40,10 @@ int main() {
         if(n2 % 10 == 0) {
             if(!feof(fp2)) {
             // 110개 이상 할당하지 않은 경우, 10개 다시 할당
-                if(cnt2 < 11) {
-                    int *new_arr2 = (int*)realloc(arr2, 10*sizeof(int));
+                if(cnt2 < 110) {
+                    int *new_arr2 = (int*)realloc(arr2, cnt1*sizeof(int));
                     arr2 = new_arr2;
-                    cnt2++;
+                    cnt2 += 10;
                 }
             }
         }
@@ -54,7 +54,6 @@ int main() {
             if(arr1[i] == arr2[j]) printf("%d ", arr1[i]);
         }
     }
-
     free(arr1);
     free(arr2);
     fclose(fp1);
