83ba138aaab30f2ffd8191acb30873c1a485e144
JunSeok <ppu1234@naver.com>
2023-03-24 18:12:04 +0900

    :art: Baekjoon 15649 practice

--- Baekjoon/0x0C/15649-2.cpp
+++ Baekjoon/0x0C/15649-2.cpp
@@ -1,28 +1,40 @@
+/*
+- 아이디어
+    - 중복을 허락하지 않는 순열 출력
+    - 수열 담을 배열 설정
+    - 중복 체크할 배열 설정
+    - 배열의 수가 m개되면 출력
+    - 아니면 1~n 까지의 수 중 중복이 안된 수로 배열에 삽입
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m, flag;
-int arr[10], pre[10];
+int n, m;
+
+int arr[10]; // 수열 담을 배열
+int isUsed[10]; // 중복 체크 배열
+
+void func(int cnt) {
+    if(cnt == m) {
+        for(int i = 0; i < m; i++) {
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    for(int i = 1; i <= n; i++) {
+        if(!isUsed[i]) {
+            arr[cnt] = i;
+            isUsed[i] = 1;
+            func(cnt+1);
+            isUsed[i] = 0;
+        }
+    }
+}
 
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin >> n >> m;
-    for(int i = 0; i < n; i++) arr[i] = i+1;
-    sort(arr, arr+n);
-    do {
-        flag = false;
-        for(int i = 0; i < m; i++) {
-            if(pre[i] != arr[i]) {
-                flag = true;
-                break;
-            }
-        }
-        if(!flag) continue;
-        for(int i = 0; i < m; i++) {
-            cout << arr[i] << ' ';
-            pre[i] = arr[i];
-        }
-        cout << '\n';
-    }while(next_permutation(arr, arr+n));
+    func(0);
 }
