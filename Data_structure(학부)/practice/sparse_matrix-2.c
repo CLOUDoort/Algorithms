8c7987b9716dcbf114997d2ebfca6e787d093a11
JunSeok <ppu1234@naver.com>
2023-03-27 12:17:44 +0900

    :art: SparseMatrix - Compress, Transpose, FastTraspose

--- Data_structure(학부)/practice/sparse_matrix-2.c
+++ Data_structure(학부)/practice/sparse_matrix-2.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 
 typedef struct _element {
-    int row;
+    int row; 
     int col;
     int value;
 } Element;
 
-void Compress(int SparseMatrix[][6], int row, int col, int cnt, Element CompMatrix[]) {
+void Compress(int SparseMatrix[][6], int row, int col, int n, Element CompMatrix[]) {
     CompMatrix[0].row = row;
     CompMatrix[0].col = col;
-    CompMatrix[0].value = cnt;
+    CompMatrix[0].value = n;
     int idx = 1;
     for(int n = 0; n < row; n++) {
         for(int m = 0; m < col; m++) {
@@ -24,14 +24,11 @@ void Compress(int SparseMatrix[][6], int row, int col, int cnt, Element CompMatr
 }
 
 void smTranspose(Element a[], Element b[]) {
-    int currentb;
-    b[0].col = a[0].row;
     b[0].row = a[0].col;
+    b[0].col = b[0].row;
     b[0].value = a[0].value;
-
-    // 총 원소 수가 1 이상일 때
     if(a[0].value > 0) {
-        currentb = 1;
+        int currentb = 1;
         for(int i = 0; i < a[0].col; i++) {
             for(int j = 1; j <= a[0].value; j++) {
                 if(a[j].col == i) {
@@ -43,17 +40,25 @@ void smTranspose(Element a[], Element b[]) {
         }
     }
 }
-void fast_transpose(Element a[], Element b[]) {
-    int row_terms[100], starting_pos[100], numCols = a[0].col, numTerms = a[0].value;
-    b[0].row = numCols;
+
+void fastTranspose(Element a[], Element b[]) {
+    int row_terms[100];
+    int starting_pos[100];
+    b[0].row = a[0].col;
     b[0].col = a[0].row;
-    b[0].value = numTerms;
-    if(numTerms > 0) {
-        for(int i = 0; i < numCols; i++) row_terms[i] = 0;
-        for(int i = 1; i <= numTerms; i++) row_terms[a[i].col]++;
+    b[0].value = a[0].value;
+    if(a[0].value > 0) {
+        for(int i = 0; i < a[0].col; i++) {
+            row_terms[i] = 0;
+        }
+        for(int i = 1; i <= a[0].value; i++) {
+            row_terms[a[i].col]++;
+        }
         starting_pos[0] = 1;
-        for(int i = 1; i < numCols; i++) starting_pos[i] = starting_pos[i-1] + row_terms[i-1];
-        for(int i = 1; i <= numTerms; i++) {
+        for(int i = 1; i < a[0].col; i++) {
+            starting_pos[i] = starting_pos[i-1]+row_terms[i-1];
+        }
+        for(int i = 1; i <= a[0].value; i++) {
             int j = starting_pos[a[i].col]++;
             b[j].row = a[i].col;
             b[j].col = a[i].row;
@@ -82,15 +87,15 @@ int main() {
     for(int i = 0; i < cnt+1; i++) {
         printf("row: %d col: %d value: %d\n", CompMatrix[i].row, CompMatrix[i].col, CompMatrix[i].value);
     }
-    Element* TransposeMatrix = (Element*)malloc(sizeof(Element)* cnt+1);
+    Element* TransposeMatrix = (Element*)malloc(sizeof(Element)*cnt+1);
     smTranspose(CompMatrix, TransposeMatrix);
-    printf("\n----------------------------------\n");
+    printf("\n------------------------------\n");
     for(int i = 0; i < cnt+1; i++) {
         printf("row: %d col : %d value: %d\n", TransposeMatrix[i].row, TransposeMatrix[i].col, TransposeMatrix[i].value);
     }
     Element* FastTransposeMatrix = (Element*)malloc(sizeof(Element)*cnt+1);
-    fast_transpose(CompMatrix, FastTransposeMatrix);
-    printf("\n----------------------------------\n");
+    fastTranspose(CompMatrix, FastTransposeMatrix);
+    printf("\n------------------------------\n");
     for(int i = 0; i < cnt+1; i++) {
         printf("row: %d col : %d value: %d\n", FastTransposeMatrix[i].row, FastTransposeMatrix[i].col, FastTransposeMatrix[i].value);
     }
