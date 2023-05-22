eb937a57fea6246be703f06b93ff494e2d0d450d
JunSeok <ppu1234@naver.com>
2023-05-22 15:28:36 +0900

    :adhesive_bandage: simple fix

--- Data_structure(학부)/18/3.c
+++ Data_structure(학부)/18/3.c
@@ -15,19 +15,24 @@ int compare(const void* a, const void *b) {
 }
 
 int main() {
-    FILE *fp = fopen("in.txt", "r");
-    if(fp == NULL) {
+    FILE *fp1 = fopen("in.txt", "r");
+    if(fp1 == NULL) {
         printf("File read error\n");
         exit(1);
     }
+    FILE *fp2 = fopen("out.txt", "w");
+    if(fp2 == NULL) {
+        printf("File open error\n");
+        exit(1);
+    }
     int cnt = 0;
-    while(fscanf(fp, "%d", &list[cnt++]) != EOF);
+    while(fscanf(fp1, "%d", &list[cnt++]) != EOF);
     cnt--;
 
     qsort(list, cnt, sizeof(int), compare);
     for(int i = 0; i < cnt; i++) {
-        printf("%d ", list[i]);
+        fprintf(fp2, "%d ", list[i]);
     }
-    fclose(fp);
+    fclose(fp1); fclose(fp2);
     return 0;
 }
