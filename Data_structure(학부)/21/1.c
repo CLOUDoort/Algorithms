794bd53aa8b0f545bfed060407dea4ef1cb27c08
JunSeok <ppu1234@naver.com>
2023-06-05 16:19:32 +0900

    :art: data_structure 0605

---
+++ Data_structure(학부)/21/1.c
@@ -0,0 +1,82 @@
+/*
+- 파일로부터 문자열 입력받아 정수로 바꾸고, 그 정수를 hashFunc에 넣어 Key값을 받는다.
+- 받은 key값을 인덱스로 하는 table에 문자열을 저장한다.
+- search 파일로부터 문자열 입력받아 정수로 바꾸고, 이전에 입력한 데이터의 정수값과 비교해서 탐색 성공과 실패를 출력한다.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_ELEMENT 10
+
+typedef struct element {
+    char data[4];
+    int key;
+} element;
+element table[MAX_ELEMENT];
+
+unsigned int stringToInt(char *key);
+int hashFunc(int key, int d);
+element search(int k, int d);
+
+int main() {
+    FILE* fp1 = fopen("in.txt", "r");
+    if(fp1 == NULL) {
+        printf("file read error\n");
+        exit(1);
+    }
+    FILE* fp2 = fopen("search.txt", "r");
+    if(fp2 == NULL) {
+        if(fp2 == NULL) {
+            printf("file read error\n");
+            exit(1);
+        }
+    }
+    int d; // hash table 크기
+    scanf("%d", &d);
+    char temp[4];
+    // insertion
+    while(fscanf(fp1, "%s", temp) != EOF) {
+        int int_number = stringToInt(temp);
+        int key = hashFunc(int_number, d);
+        table[key].key = int_number;
+        strcpy(table[key].data, temp);
+    }
+
+    printf("\n");
+    while(fscanf(fp2, "%s", temp) != EOF) {
+        int search_number = stringToInt(temp);
+        int flag = 0;
+        for(int i = 0; i < d; i++) {
+            if(table[i].key == search_number) {
+                printf("S\n");
+                flag = 1;
+            }
+        }
+        if(!flag) printf("E\n");
+    }
+
+    printf("\n");
+    for(int i = 0; i < d; i++) {
+        if(table[i].key) {
+            printf("%d: %s (%d)\n",i, table[i].data, table[i].key);
+        }
+        else printf("%d: \n", i);
+    }
+
+    fclose(fp1); fclose(fp2);
+    return 0;
+}
+
+
+int hashFunc(int key, int d) {
+    return key%d;
+}
+
+unsigned int stringToInt(char *key) {
+    int number = 0;
+    while(*key) {
+        number += *key++;
+    }
+    return number;
+}
---
+++ Data_structure(학부)/21/2.c
@@ -0,0 +1,110 @@
+/*
+- element 포인터 배열을 선언하고 Node 만들어 삽입한다.
+- 데이터를 Key값에 맞춰 삽입하는데, Null이 나오면 삽입하고, 데이터가 반환되면 꽉 찼다고 말해준다.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_ELEMENT 10
+
+typedef struct element* elementPointer;
+typedef struct element {
+    char data[4];
+    int key;
+    elementPointer link;
+} element;
+elementPointer ht[MAX_ELEMENT];
+
+elementPointer createNode();
+int hashFunc(int key, int d);
+unsigned int stringToInt(char *key);
+elementPointer search(int k, int d);
+
+int main() {
+    FILE* fp1 = fopen("in.txt", "r");
+    if(fp1 == NULL) {
+        printf("file read error\n");
+        exit(1);
+    }
+    FILE* fp2 = fopen("search.txt", "r");
+    if(fp2 == NULL) {
+        if(fp2 == NULL) {
+            printf("file read error\n");
+            exit(1);
+        }
+    }
+
+    int d;
+    scanf("%d", &d);
+    char temp[4];
+    for(int i = 0; i < d; i++) {
+        elementPointer newNode = createNode();
+        ht[i] = newNode;
+    }
+    while(fscanf(fp1, "%s", temp) != EOF) {
+        int num = stringToInt(temp);
+        int key = hashFunc(num, d);
+        if(!search(num, d)) {
+            strcpy(ht[key]->data, temp);
+            ht[key]->key = num;
+        }
+        else {
+            printf("table full\n");
+            exit(1);
+        }
+    }
+    printf("\n");
+    while(fscanf(fp2, "%s", temp) != EOF) {
+        int search_number = stringToInt(temp);
+        int flag = 0;
+        for(int i = 0; i < d; i++) {
+            if(ht[i]->key == search_number) {
+                printf("S\n");
+                flag = 1;
+            }
+        }
+        if(!flag) printf("E\n");
+    }
+
+    printf("\n");
+    for(int i = 0; i < d; i++) {
+        if(ht[i]->key) {
+            printf("%d: %s (%d)\n",i, ht[i]->data, ht[i]->key);
+        }
+        else printf("%d: \n", i);
+    }
+
+    fclose(fp1); fclose(fp2);
+    return 0;
+}
+
+elementPointer createNode() {
+    elementPointer newNode = (elementPointer)malloc(sizeof(*newNode));
+    if(newNode == NULL) {
+        printf("Malloc error\n");
+        exit(1);
+    }
+    return newNode;
+}
+
+elementPointer search(int k, int d) {
+    elementPointer current;
+    int homeBucket = hashFunc(k, d);
+    for(current = ht[homeBucket]; current; current = current->link) {
+        if(current->key == k) return current;
+    }
+    return NULL;
+}
+
+int hashFunc(int key, int d) {
+    return key%d;
+}
+
+unsigned int stringToInt(char *key) {
+    int number = 0;
+    while(*key) {
+        number += *key++;
+    }
+    return number;
+}
