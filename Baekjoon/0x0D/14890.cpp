0e0634b73799986145e283fd3b2dc6a1fa4ad2cc
JunSeok <ppu1234@naver.com>
2023-06-18 21:52:38 +0900

    :art: Baekjoon 14890

--- Baekjoon/0x0D/14890.cpp
+++ Baekjoon/0x0D/14890.cpp
@@ -1,89 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n,l, board[102][102];
-int ans;
-vector<pair<int, int>> line;
+int n, l;
+int board[102][102];
 
-void row_check(int x, int y) {
-    int length = 1;
-    int shift = 0;
-    for(int i = 0; i < n-1; i++) {
-        if(board[x][i] == board[x][i+1]) {
-            // 경사로 길이
-            if(shift) {
-                shift++;
-                if(i == n-2) {
-                    if(shift < l) return;
-                }
-            }
-            else length++;
+bool check(vector<int>& line) {
+    int idx = 0;
+    int cnt = 1;
+    while(idx < n-1) {
+        if(abs(line[idx+1] - line[idx]) > 1) return 0;
+        if(line[idx] == line[idx+1]) {
+            cnt++;
+            idx++;
         }
-        else if(board[x][i] > board[x][i+1]) {
-            if(abs(board[x][i]-board[x][i+1]) > 1)  return;
-            if(shift) {
-                if(shift < l) return;
-                shift = 0;
-            }
-            if(i == n-2) {
-                if(shift < l) return;
-            }
-            length = 1;
-            shift = 1;
+        else if(line[idx] < line[idx+1]) {
+            if(cnt < l) return 0;
+            cnt = 1;
+            idx++;
         }
         else {
-            if(abs(board[x][i]-board[x][i+1]) > 1) return;
-            if(shift) {
-                if(l == 1 && shift == 1) return;
-                if(shift < l) return;
-                shift = 0;
+            if(idx + l >= n) return 0;
+            for(int i = idx+1; i < idx+l; i++) {
+                if(line[i] != line[i+1]) return 0;
             }
-            if(length < l) return;
-            length = 1;
+            idx = idx+l;
+            cnt = 0;
         }
     }
-    ans++;
-    // cout << "row: (" << x << " " << y << ")\n";
-}
-void col_check(int x, int y) {
-    int length = 1;
-    int shift = 0;
-    for(int i = 0; i < n-1; i++) {
-        if(board[i][y] == board[i+1][y]) {
-            // 경사로 길이
-            if(shift) {
-                shift++;
-                if(i == n-2) {
-                    if(shift < l) return;
-                }
-            }
-            else length++;
-        }
-        else if(board[i][y] > board[i+1][y]) {
-            if(abs(board[i][y]-board[i+1][y]) > 1)  return;
-            if(shift) {
-                if(shift < l) return;
-                shift = 0;
-            }
-            length = 1;
-            shift = 1;
-            if(i == n-2) {
-                if(shift < l) return;
-            }
-        }
-        else {
-            if(abs(board[i][y]-board[i+1][y]) > 1) return;
-            if(shift) {
-                if(l == 1 && shift == 1) return;
-                if(shift < l) return;
-                shift = 0;
-            }
-            if(length < l) return;
-            length = 1;
-        }
-    }
-    ans++;
-    // cout << "col: (" << x << " " << y << ")\n";
+    return 1;
 }
 
 int main(void) {
@@ -95,13 +39,20 @@ int main(void) {
             cin >> board[i][j];
         }
     }
+    int ans = 0;
+
     for(int i = 0; i < n; i++) {
-        line.push_back({i, 0});
+        vector<int> line;
+        for(int j = 0; j < n; j++) line.push_back(board[i][j]);
+        ans += check(line);
     }
+
     for(int i = 0; i < n; i++) {
-        int x, y; tie(x, y) = line[i];
-        row_check(x, y);
-        col_check(y, x);
+        vector<int> line;
+        for(int j = 0; j < n; j++) line.push_back(board[j][i]);
+        ans += check(line);
     }
+
+
     cout << ans;
 }
