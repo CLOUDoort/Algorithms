238e516d29afcde3808a5baf9c66971099608286
JunSeok <ppu1234@naver.com>
2023-06-28 13:26:13 +0900

    :art: Baekjoon 10814

---
+++ Baekjoon/0x0E(정렬)/10814.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+typedef struct {
+    int age;
+    string name;
+} user;
+
+user arr[100001], temp[100001];
+
+void merge(int st, int en) {
+    int mid = (st+en)/2;
+    int lidx = st, ridx = mid;
+    for(int i = st; i < en; i++) {
+        if(ridx == en) temp[i] = arr[lidx++];
+        else if(lidx == mid) temp[i] = arr[ridx++];
+        else if(arr[lidx].age <= arr[ridx].age) temp[i] = arr[lidx++];
+        else temp[i] = arr[ridx++];
+    }
+    for(int i = st; i < en; i++) arr[i] = temp[i];
+}
+
+void merge_sort(int st, int en) {
+    if(en <= st+1) return;
+    int mid = (st+en)/2;
+    merge_sort(st, mid);
+    merge_sort(mid, en);
+    merge(st, en);
+}
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i].age >> arr[i].name;
+    }
+    merge_sort(0, n);
+    for(int i = 0; i < n; i++) {
+        cout << arr[i].age << ' ' << arr[i].name << '\n';
+    }
+}
