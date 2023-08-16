e8f13ac7c2aa4b28bd8f35226819e4543a4fde99
JunSeok <ppu1234@naver.com>
2023-08-16 13:53:02 +0900

    :art: Baekjoon 12865

---
+++ Baekjoon/0x10(DP)/12865.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+
+int w[102], v[102], d[100002];
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n, k; cin >> n >> k;
+    for(int i = 1; i <= n; i++) cin >> w[i] >> v[i];
+    for(int i = 1; i <= n; i++) {
+        for(int j = k; j >= 1; j--) {
+            if(w[i] <= j) {
+                d[j] = max(d[j], d[j-w[i]]+v[i]);
+            }
+        }
+    }
+    cout << d[k];
+}
