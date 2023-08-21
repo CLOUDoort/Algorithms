d068ec1ee6eb605314e1eee8dfb9bc7c90c756ff
JunSeok <ppu1234@naver.com>
2023-08-21 14:10:58 +0900

    :art: Baekjoon 1477

--- Baekjoon/1477.cpp
+++ Baekjoon/1477.cpp
@@ -1,35 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int a[1002], btw[502];
-bool comp(int a, int b) {
-    return a > b;
+int n, m, l;
+int v[51];
+
+bool solve(int x) {
+    int must_cnt = 0;
+    int pre = 0;
+    for(int i = 0; i <= n; i++) {
+        int dist = v[i] - pre;
+        if(dist >= x) {
+            if(dist % x != 0) must_cnt += dist / x;
+            else must_cnt += (dist / x-1);
+        }
+        pre = v[i];
+    }
+    if(must_cnt > m) return true;
+    else return false;
 }
 
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int n, m, l; cin >> n >> m >> l;
-    for(int i = 0; i < n; i++){
-        int t; cin >> t;
-        a[t]++;
+    cin >> n >> m >> l;
+    for(int i = 0; i < n; i++) cin >> v[i];
+    sort(v, v+n);
+    v[n] = l;
+
+    int st = 1, en = l;
+    while(st < en) {
+        int mid = (st+en)/2;
+        if(solve(mid)) st = mid+1;
+        else en = mid;
     }
-    int x = 0, y = 0, idx = 1, st = 0;
-    for(int i = 1; i <= l; i++) {
-        if(!a[i]) continue;
-        if(!x) {
-            st = i;
-            x = i;
-        }
-        else if(!y) y = i;
-        if(x && y) {
-            btw[idx++] = y-x; 
-            x = y; y = 0;
-        }
-    }
-    btw[0] = st;
-    btw[idx] = l-x;
-    sort(btw, btw+idx+1, comp);
-    if(m == 1) cout << btw[1]/2+1;
-    else cout << btw[1]/(m-idx+2);
+    cout << st;
+    return 0;
 }
