d617d03b4bec57316494eb9c2078d525332cc8c3
JunSeok <ppu1234@naver.com>
2023-08-29 15:06:58 +0900

    :art: Baekjoon 1541

--- Baekjoon/0x11(그리디)/1541.cpp
+++ Baekjoon/0x11(그리디)/1541.cpp
@@ -1,25 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int tmp, ans;
+int ans, tmp;
 int sign = 1;
+
 int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
-
   string input;
   cin >> input;
-  for (char c: input){
-    if (c == '+' || c == '-'){
-      ans += tmp * sign;
-      if (c == '-') sign = -1;
+  for(char c : input) {
+    if(c == '+' || c == '-') {
+      ans += tmp*sign;
+      if(c == '-') sign = -1;
       tmp = 0;
     }
-    else{
+    else {
       tmp *= 10;
       tmp += c - '0';
     }
   }
-  ans += tmp * sign;
+  ans += tmp*sign;
   cout << ans;
 }
