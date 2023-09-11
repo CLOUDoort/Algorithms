0dd01705883ae645e34cd1d07ff269c515e28555
JunSeok <ppu1234@naver.com>
2023-09-11 21:24:24 +0900

    :art: greatest common divisor

--- Baekjoon/0x12(수학)/GCD.cpp
+++ Baekjoon/0x12(수학)/GCD.cpp
@@ -5,14 +5,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int GDC(int a, int b) {
+int gcd(int a, int b) {
     if(a == 0) return b;
-    return GDC(a%b, a);
+    return gcd(a%b, a);
 }
 
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
     int a, b; cin >> a >> b;
-    cout << GDC(a, b);
+    cout << gcd(a, b);
 }
