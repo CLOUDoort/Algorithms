2e3eecd8606d60efc15b16b9acc64e1a7c520d2e
JunSeok <ppu1234@naver.com>
2023-09-16 20:54:36 +0900

    :art: Baekjoon 1038

--- Baekjoon/0x12(수학)/1038.cpp
+++ Baekjoon/0x12(수학)/1038.cpp
@@ -1,29 +1,28 @@
-// Authored by : scsc3204
-// Co-authored by : -
-// http://boj.kr/2300d12c7a24426a97284f983d31b3fa
+/*
+    - 중복없음, 유일 수 => 조합 사용
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
 vector<bool> a(10, 0);
 
-int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-
-  int cnt = 0;
-
-  int n; cin >> n;
-  for(int i = 1; i <= 10; i++) {
-    a[10 - i] = 1;
-    do {
-      if(cnt != n) {
-        cnt++;
-        continue;
-      }
-      for(int i = 0; i < 10; i++)
-        if(a[i]) cout << 9 - i;
-      return 0;
-    } while(next_permutation(a.begin(), a.end()));
-  }
-  cout << -1;
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n, cnt = 0; cin >> n;
+    for(int i = 1; i <= 10; i++) {
+        a[10-i] = 1;
+        do {
+            if(cnt != n) {
+                cnt++;
+                continue;
+            }
+            for(int i = 0; i < 10; i++) {
+                if(a[i]) cout << 9-i;
+            }
+            return 0;
+        } while(next_permutation(a.begin(), a.end()));
+    }
+    cout << -1;
 }
