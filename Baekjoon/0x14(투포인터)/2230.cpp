f89cf50af30a52c46e769d9ee4d201b2ddf4b12c
JunSeok <ppu1234@naver.com>
2023-10-03 15:00:35 +0900

    :art: Baekjoon 2230 binary search

--- Baekjoon/0x14(투포인터)/2230.cpp
+++ Baekjoon/0x14(투포인터)/2230.cpp
@@ -1,21 +1,22 @@
+// 이분탐색
+
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m;
 int a[100005];
 int mn = 0x7fffffff;
 
-int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cin >> n >> m;
-  for(int i = 0; i < n; i++) cin >> a[i];
-  sort(a, a+n);
-  int en = 0;
-  for(int st = 0; st < n; st++){
-    while(en < n && a[en] - a[st] < m) en++;
-    if(en == n) break; // en이 범위를 벗어날 시 종료
-    mn = min(mn, a[en] - a[st]);
-  }
-  cout << mn;
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n, m; cin >> n >> m;
+    for(int i = 0; i < n; i++) cin >> a[i];
+    sort(a, a+n);
+    for(int i = 0; i < n; i++) {
+        int idx = lower_bound(a, a+n, a[i]+m)-a;
+        if(idx != n) {
+            mn = min(mn, a[idx]-a[i]);
+        }
+    }
+    cout << mn;
 }
