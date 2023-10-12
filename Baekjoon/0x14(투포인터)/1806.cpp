b1289995edfd30821027afba7a73a91e558c0944
JunSeok <ppu1234@naver.com>
2023-10-12 13:11:59 +0900

    :art: Baekjoon 1806 practice

--- Baekjoon/0x14(투포인터)/1806.cpp
+++ Baekjoon/0x14(투포인터)/1806.cpp
@@ -9,17 +9,16 @@ int main(void) {
   int n, s; cin >> n >> s;
   for(int i = 0; i < n; i++) cin >> a[i];
   int mn = 0x7fffffff;
-  int en = 0;
-  int tot = a[0];
-  for(int st = 0; st < n; st++) {
+  int tot = a[0], en = 0;
+  for(int i = 0; i < n; i++) {
     while(en < n && tot < s) {
       en++;
       if(en != n) tot += a[en];
     }
     if(en == n) break;
-    mn = min(mn, en-st+1);
-    tot -= a[st];
+    tot -= a[i];
+    mn = min(mn, en-i+1);
   }
   if(mn == 0x7fffffff) mn = 0;
-  cout << mn;  
+  cout << mn;
 }
