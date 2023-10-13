0fc7dd61354c190efa379ac4548f0c7c90f01552
JunSeok <ppu1234@naver.com>
2023-10-13 23:39:33 +0900

    :art: Baekjoon 1644 practice

--- Baekjoon/0x14(투포인터)/1644.cpp
+++ Baekjoon/0x14(투포인터)/1644.cpp
@@ -1,28 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MXN = 4000002;
-vector<bool> seive(MXN, true);
+const int MAN = 4000002;
+
+vector<bool> state(MAN, true);
 vector<int> primes;
+
 int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
-  for(int i=2; i*i<MXN; i++){
-    if (!seive[i]) continue;
-    for (int j = i * i; j < MXN; j += i) 
-      seive[j] = false;
+  state[1] = false;
+  for(int i = 2; i*i < MAN; i++) {
+    if(!state[i]) continue;
+    for(int j = i*i; j < MAN; j += i) {
+      state[j] = false;
+    }
   }
-  for (int i = 2; i < MXN; i++) if (seive[i]) primes.push_back(i);
+  for(int i = 2; i < MAN; i++) {
+    if(state[i]) primes.push_back(i);
+  }
   primes.push_back(0);
-
   int target, s = 0, e = 1, ans = 0, tmpSum = primes[0];
   cin >> target;
-  while (1) {
-    if (tmpSum == target) ans++;
-    if (tmpSum <= target) tmpSum += primes[e++];
-    if (target < tmpSum) tmpSum -= primes[s++];
-    if (e == int(primes.size())) break;
+  while(1) {
+    if(tmpSum == target) ans++;
+    if(tmpSum <= target) tmpSum += primes[e++];
+    if(target < tmpSum) tmpSum -= primes[s++];
+    if(e == int(primes.size())) break;
   }
   cout << ans;
 }
